8ed65ff3e13c9f2233e9109fcd915c26dcad573a
Piotr Gadzinski <piotr@intelestream.net>
2020-05-25 18:22:58 +0200

    F-01: Use case test refactoring

--- tests/Feature/ListTasksOwnedByUserFeatureTest.php
+++ tests/Feature/ListTasksOwnedByUserFeatureTest.php
@@ -10,29 +10,29 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class TaskTest extends TestCase
+class ListTasksOwnedByUserFeatureTest extends TestCase
 {
-
     use RefreshDatabase;
 
     /**
      * It can list tasks owned by the user
      *
+     * @return void
      * @test
      */
-    public function it_can_list_tasks_owned_by_user()
+    public function it_can_list_tasks_owned_by_user() : void
     {
         $user = factory(User::class)->create();
         $task = $user->tasks()->create(factory(Task::class)->raw());
         
         $response = $this->be($user, 'api')
             ->getJson('/api/v1/tasks');
-
+        
         $response->assertStatus(200)
-            ->assertJson([
-                'data' => [
-                    $task->toArray()
-                ]
+            ->assertJsonFragment([
+                'title' => $task->title,
+                'description' => $task->description,
+                'user_id' => (string) $user->id,
             ]);
     }
 }
