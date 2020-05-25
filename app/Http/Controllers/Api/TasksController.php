733e44576d003b387ebfbe2b4f0050047e4e981f
Piotr Gadzinski <piotr@intelestream.net>
2020-05-25 18:22:43 +0200

    F-01: Use case controller refactoring

--- app/Http/Controllers/Api/TasksController.php
+++ app/Http/Controllers/Api/TasksController.php
@@ -4,61 +4,28 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Requests\ListTasksOwnedByUserRequestInterface;
+use App\UseCases\ListTasksOwnedByUserInteractorInterface;
 
 class TasksController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+     * @param Request $httpRequest
+     * @param ListTasksOwnedByUserRequestInterface $dto
+     * @param ListTasksOwnedByUserInteractorInterface $useCase
+     * @return JsonResponse
+    */
+    public function index(Request $httpRequest, ListTasksOwnedByUserRequestInterface $dto, ListTasksOwnedByUserInteractorInterface $useCase) : JsonResponse
     {
-        return auth()->user()->tasks()->paginate();
-    }
+        $user = $httpRequest->user();
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+        $dto->setUser($user);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+        $response = $useCase->run($dto);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return response()->json($response->getTasks());
     }
 }
