fc9b2e14c66e9f1852f19ebce28d9be416b7fe03
Piotr Gadzinski <piotr@intelestream.net>
2020-05-26 11:23:31 +0200

    F-01: Feature update to hide completed tasks test change

--- tests/Feature/ListTasksOwnedByUserFeatureTest.php
+++ tests/Feature/ListTasksOwnedByUserFeatureTest.php
@@ -9,6 +9,7 @@ use App\Models\Task;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Carbon\Carbon;
 
 class ListTasksOwnedByUserFeatureTest extends TestCase
 {
@@ -33,6 +34,38 @@ class ListTasksOwnedByUserFeatureTest extends TestCase
                 'title' => $task->title,
                 'description' => $task->description,
                 'user_id' => (string) $user->id,
+            ]);
+    }
+
+    /**
+     * It does not list tasks that are completed
+     *
+     * @return void
+     * @test
+     */
+    public function it_can_list_only_not_completed_tasks() : void
+    {
+        $user = factory(User::class)->create();
+        
+        $taskNotDone = factory(Task::class)->create();
+        $taskDone = factory(Task::class)->create();
+        $taskDone->completed_at = Carbon::yesterday();
+
+        $user->tasks()->save($taskDone);
+        $user->tasks()->save($taskNotDone);
+        
+        $response = $this->be($user, 'api')
+            ->getJson('/api/v1/tasks');
+        
+        $response->assertStatus(200)
+            ->assertJsonFragment([
+                'title' => $taskNotDone->title,
+                'description' => $taskNotDone->description,
+                'user_id' => (string) $user->id,
+            ])
+            ->assertJsonMissing([
+                'title' => $taskDone->title,
+                'description' => $taskDone->description,
             ]);
     }
 }
